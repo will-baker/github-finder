6c4022ba75a51ab2295f8d8430725d1aeedeaa52
will-baker <will.baker3@icloud.com>
2020-08-22 13:46:36 +1200

    Section 4 begin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    // make request to api when component loads here
+    // console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Search />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+
+        <h1>Hello from react</h1>
+      </div>
+
+      // Has to have one parent element
+
+      // if dont want app div - just h1/h2, replace <div> with <React.fragment>
+    );
+  }
 }
 
 export default App;
