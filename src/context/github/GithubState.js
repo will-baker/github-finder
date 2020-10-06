6be554b6aaee0a3b4c98af9a709450a2b35f8879
will-baker <will.baker3@icloud.com>
2020-10-06 23:42:05 +1300

    Finished section 5 - moved to use components and app level state

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -35,22 +35,49 @@ const GithubState = (props) => {
   };
 
   // Get user
+  const getUser = async (username) => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data,
+    });
+  };
 
   // Get repos
+  const getUserRepos = async (username) => {
+    setLoading(true);
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  };
 
   // Clear Users
-
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
   // Set loading
   const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider
       value={{
-        users: state.user,
+        users: state.users,
         user: state.user,
         repos: state.repos,
         loading: state.loading,
         searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
       }}
     >
       {props.children}
